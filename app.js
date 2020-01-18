57dcfb533d0a75aa11dad6dd3280fed12ece351d
Kathryn Xing <kxxing@edu.uwaterloo.ca>
2020-01-18 16:48:08 -0500

    test first personal

--- app.js
+++ app.js
@@ -2,10 +2,9 @@ var express = require("express");
 var app = express();
 app.use(express.static("public"));
 app.set("view engine", "ejs"); //to eliminate writnig .ejs every time
-
+//routes
 app.get("/",function(req,res){
-    //res.render("home");
-    res.render("home");
+    res.render("home");// home.ejs
 });
 var port = process.env.PORT || 3000;
 app.listen(port, function () {
