478dd4036f3613e02ff4d4885e6fbc02f9dd633c
Kathryn Xing <kxxing@edu.uwaterloo.ca>
2020-01-28 09:39:23 -0500

    clearbanc video page

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 var express = require("express");
 var app = express();
-app.use(express.static("assets"));
+app.use(express.static("assets"));//when reference allways go to /assets/...
 app.set("view engine", "ejs"); //to eliminate writnig .ejs every time
 //routes
 app.get("/",function(req,res){
     res.render("home");// home.ejs
+});
+app.get("/clearbancHireMe",function(req,res){
+    res.render("clearbanc");//clearbanc.ejs
 });
 var port = process.env.PORT || 3000;
 app.listen(port, function () {
