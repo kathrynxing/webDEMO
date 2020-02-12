02b83de1052f4c4431692e8f2bc86518c7c00b03
Kathryn Xing <kxxing@edu.uwaterloo.ca>
2020-02-11 21:42:54 -0500

    DEMO WhereToStudy

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 var express = require("express");
 var app = express();
+var bodyParser = require("body-parser");
+
+app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("assets"));//when reference allways go to /assets/...
 app.set("view engine", "ejs"); //to eliminate writnig .ejs every time
 //routes
@@ -8,6 +11,28 @@ app.get("/",function(req,res){
 });
 app.get("/clearbancHireMe",function(req,res){
     res.render("clearbanc");//clearbanc.ejs
+});
+
+/** WhereToStudy DEMO */
+var studySpaces = [
+    {name: "SLC", image: "imgs/pattern1.jpeg" },
+    {name: "St. Paul", image: "imgs/pattern2.jpeg"}
+]
+
+app.get("/WhereToStudyDEMO",function(req,res){
+    res.render("WhereToStudylanding", {Spaces:studySpaces});
+
+});
+app.get("WhereToStudyDEMO/studyspaces",function(req,res){
+    res.render("WhereToStudylanding", {Spaces:studySpaces});
+});
+app.post("/WhereToStudyDEMO/studyspaces",function(req,res){
+    res.send("this is spaces to be filled");
+});
+
+
+app.get("WhereToStudyDEMO/studyspaces/new",function(req,res){
+    res.send("a form goes here");
 });
 var port = process.env.PORT || 3000;
 app.listen(port, function () {
