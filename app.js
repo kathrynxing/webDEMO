2b3ddf050c04d1f2ccc9cf520d930369eb821ae2
Kathryn Xing <kxxing@edu.uwaterloo.ca>
2020-02-11 22:14:47 -0500

    post request working

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var app = express();
 var bodyParser = require("body-parser");
 
 app.use(bodyParser.urlencoded({extended: true}));
+
 app.use(express.static("assets"));//when reference allways go to /assets/...
 app.set("view engine", "ejs"); //to eliminate writnig .ejs every time
 //routes
@@ -15,24 +16,28 @@ app.get("/clearbancHireMe",function(req,res){
 
 /** WhereToStudy DEMO */
 var studySpaces = [
-    {name: "SLC", image: "imgs/pattern1.jpeg" },
-    {name: "St. Paul", image: "imgs/pattern2.jpeg"}
+    {name: "SLC", image: "/imgs/pattern1.jpeg" },
+    {name: "St. Paul", image: "/imgs/pattern2.jpeg"}
 ]
 
 app.get("/WhereToStudyDEMO",function(req,res){
     res.render("WhereToStudylanding", {Spaces:studySpaces});
 
 });
-app.get("WhereToStudyDEMO/studyspaces",function(req,res){
+app.get("/WhereToStudyDEMO/studyspaces",function(req,res){
     res.render("WhereToStudylanding", {Spaces:studySpaces});
 });
 app.post("/WhereToStudyDEMO/studyspaces",function(req,res){
-    res.send("this is spaces to be filled");
+   // res.send("this is spaces to be filled");
+    var name = req.body.name;
+    var img = req.body.image;
+    var newSpace = {name:name, image:img};
+    studySpaces.push(newSpace);
+    res.redirect("/WhereToStudyDEMO/studyspaces");
 });
 
-
-app.get("WhereToStudyDEMO/studyspaces/new",function(req,res){
-    res.send("a form goes here");
+app.get("/WhereToStudyDEMO/studyspaces/new",function(req,res){
+    res.render("new");
 });
 var port = process.env.PORT || 3000;
 app.listen(port, function () {
